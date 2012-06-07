30448591a0afd8b78900aee24320e2ec0cf576d9
Florian Lengyel <florian.lengyel@gmail.com>
2012-06-07 07:08:16 -0400

    Check on bounding box -- should check if identical to grid squares and if length corresponds to 1 arc minute.

--- niger.py
+++ niger.py
@@ -11,8 +11,12 @@ if __name__ == '__main__':
     rec0 = sr[0]
     print "shapeType: ", rec0.shape.shapeType
     print len(rec0.shape.points), "points: ", rec0.shape.points
+    print "bbox:", rec0.shape.bbox
     print len(rec0.record), "record: ", rec0.record
     for sr in sh.shapeRecords():
+        shp = sr.shape
+        print shp.bbox
+        print shp.bbox[2]-shp.bbox[0],shp.bbox[3]-shp.bbox[1]
 	print "ID: ", sr.record[7]
 	print "NAME:", sr.record[8]
 	print "UPSTREAM:", sr.record[9]
