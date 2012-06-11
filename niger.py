9d1f39d3804e986f90056785ff8c5e07fff54772
Florian Lengyel <florian.lengyel@gmail.com>
2012-06-11 17:35:33 -0400

    Passes 1 and 2 added. Some target shape values are negative!

--- niger.py
+++ niger.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 import shapefile
 
+def latlonstr(lat,lon):
+    fspec = '%.17f'
+    return fspec % lat + ',' + fspec % lon
+
+def shapeID(sr):
+    return sr.record[7]	# a magic number
+
+def pass1(shapes):
+    dct = dict()
+    for rec in shapes.shapeRecords():
+	bbox = rec.shape.bbox
+	tgt = latlonstr(bbox[2],bbox[3])
+	dct[tgt] = [shapeID(rec)]    # target goes to segment shape ID and empty list
+	print tgt, dct[tgt]
+    return dct
+
+def pass2(shapes, dct):
+    for rec in shapes.shapeRecords():
+	bbox = rec.shape.bbox
+	src = latlonstr(bbox[0],bbox[1])
+	if src in dct:
+	    IDlist = dct[src]
+            IDlist.append(shapeID(rec)) # gotcha: this modifies the list and returns none
+	    dct[src] = IDlist 
+ 	    print src, dct[src]
+    return dct	 
+	
+
 if __name__ == '__main__':
+    print latlonstr(10.0102020201012,6.123456789012345)
     sh = shapefile.Reader("Niger_River_Active_1min.shp")
     print "Loading shapes..."
+    dct = pass1(sh)
+    pass2(sh, dct)
+    exit(0)
     shape = sh.shapes()
     print len(sh.fields), " fields: ", sh.fields
     print "Loading records..."
