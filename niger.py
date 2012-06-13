e4af39df56dc823f7914d7fc630d9b9ba34ac57b
Florian Lengyel <florian.lengyel@gmail.com>
2012-06-12 23:31:45 -0400

    Preprocessing and preparation of dictionaries for Niger river network done. Use loadmaps("NigerRiverDict") to load the ID to upstream ID list dictionary and the ID to [[upstream coord],[downstream coord]] dictionary. pickle changed to cPickle. loadmaps is not overwhelmingly slow.

--- niger.py
+++ niger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+# preprocess ESRI shape file for 1min resolution niger river network
 import shapefile
 import cPickle as pickle     # dump and load data structures
-from pprint import pprint
 
 # !EEE 64 bit numbers
 def latlonstr(lat,lon):
@@ -10,9 +10,6 @@ def latlonstr(lat,lon):
 
 def shapeID(sr):
     return sr.record[7]	# a magic number 
-
-def downstreamID(sr):   
-    return sr.record[14] # This magic number refers to TRAVEL
 
 # the points [[a,b],[c,d]] data structure represents
 # a vector from the upstream coordinate to the downstream
@@ -58,37 +55,19 @@ def pass3(dct):
 def preprocess(shapefilename):
     sh = shapefile.Reader(shapefilename)
     maps = pass1(sh)
-    return (pass3(pass2(sh, maps[0])), maps[1])
+    return (pass3(pass2(sh, maps[0])), maps[1]) # id -> [upstream list], id -> [[upstream coords[,[downstream coords]]
+
+# the breadth first search should import this
+def loadmaps(pickleFile):
+    mapfile = open(pickleFile, "r")
+    maps =  pickle.load(mapfile)
+    mapfile.close()
+    return maps
 
 if __name__ == '__main__':
     maps = preprocess("Niger_River_Active_1min.shp")
-    mapfile = open("NigerDictionary", "w")  # write dictionaries
+    mapfile = open("NigerRiverDict", "w")  # write dictionaries
     pickle.dump(maps, mapfile)
     mapfile.close()
-
-    print "Written to disk. Delete the map data structure."
-    del maps
-    print "Read back"
-
-    mapfile = open("NigerDictionary.pck", "r")
-    newmaps = pickle.load(mapfile)
-    pprint(newmaps[0])
-    pprint(newmaps[1])
     exit(0)
 
-    shape = sh.shapes()
-    print len(sh.fields), " fields: ", sh.fields
-    print "Loading records..."
-    sr = sh.shapeRecords()
-    rec0 = sr[0]
-    print "shapeType: ", rec0.shape.shapeType
-    print len(rec0.shape.points), "points: ", rec0.shape.points
-    print "bbox:", rec0.shape.bbox
-    print len(rec0.record), "record: ", rec0.record
-    for sr in sh.shapeRecords():
-        shp = sr.shape
-        print shp.bbox
-        print shp.bbox[2]-shp.bbox[0],shp.bbox[3]-shp.bbox[1]
-	print "ID: ", sr.record[7]
-	print "NAME:", sr.record[8]
-	print "UPSTREAM:", sr.record[9]
