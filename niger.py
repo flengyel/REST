5b6123e2d098869ef83817bd892defa7ce5ab6d8
Florian Lengyel <florian.lengyel@gmail.com>
2012-06-12 22:51:15 -0400

    Passes 1, 2 and 3 added. Attempting to save and read dictionaries as pickle files -- but something like sqlalchemy may be necessary

--- niger.py
+++ niger.py
@@ -1,41 +1,81 @@
 #!/usr/bin/python
 import shapefile
+import cPickle as pickle     # dump and load data structures
+from pprint import pprint
 
+# !EEE 64 bit numbers
 def latlonstr(lat,lon):
-    fspec = '%.17f'
+    fspec = '%.15f'
     return fspec % lat + ',' + fspec % lon
 
 def shapeID(sr):
-    return sr.record[7]	# a magic number
+    return sr.record[7]	# a magic number 
 
+def downstreamID(sr):   
+    return sr.record[14] # This magic number refers to TRAVEL
+
+# the points [[a,b],[c,d]] data structure represents
+# a vector from the upstream coordinate to the downstream
+# coordinate
+
+# create key based on upstream point
+def upstream(rec):
+    latlon = rec.shape.points[0]
+    return latlonstr(latlon[0],latlon[1])
+
+def downstream(rec):
+    latlon = rec.shape.points[1]
+    return latlonstr(latlon[0],latlon[1])
+
+# create initial upstream coordinate -> cell map
+# and cell -> coordinate pair map
 def pass1(shapes):
     dct = dict()
+    pntMap = dict()
     for rec in shapes.shapeRecords():
-	bbox = rec.shape.bbox
-	tgt = latlonstr(bbox[2],bbox[3])
-	dct[tgt] = [shapeID(rec)]    # target goes to segment shape ID and empty list
-	print tgt, dct[tgt]
-    return dct
+	tgt = upstream(rec)
+        sID = shapeID(rec)
+	dct[tgt] = [sID]    # target goes to segment shape ID and empty list
+        pntMap[sID] = rec.shape.points
+    return (dct, pntMap)
 
 def pass2(shapes, dct):
     for rec in shapes.shapeRecords():
-	bbox = rec.shape.bbox
-	src = latlonstr(bbox[0],bbox[1])
+	src = downstream(rec)
 	if src in dct:
 	    IDlist = dct[src]
             IDlist.append(shapeID(rec)) # gotcha: this modifies the list and returns none
 	    dct[src] = IDlist 
- 	    print src, dct[src]
     return dct	 
 	
+def pass3(dct):
+    idmap = dict()
+    for item in dct:
+      IDlist = dct[item]
+      idmap[IDlist[0]] = IDlist[1:]   # make the first map to the rest
+    return idmap
+
+def preprocess(shapefilename):
+    sh = shapefile.Reader(shapefilename)
+    maps = pass1(sh)
+    return (pass3(pass2(sh, maps[0])), maps[1])
 
 if __name__ == '__main__':
-    print latlonstr(10.0102020201012,6.123456789012345)
-    sh = shapefile.Reader("Niger_River_Active_1min.shp")
-    print "Loading shapes..."
-    dct = pass1(sh)
-    pass2(sh, dct)
+    maps = preprocess("Niger_River_Active_1min.shp")
+    mapfile = open("NigerDictionary", "w")  # write dictionaries
+    pickle.dump(maps, mapfile)
+    mapfile.close()
+
+    print "Written to disk. Delete the map data structure."
+    del maps
+    print "Read back"
+
+    mapfile = open("NigerDictionary.pck", "r")
+    newmaps = pickle.load(mapfile)
+    pprint(newmaps[0])
+    pprint(newmaps[1])
     exit(0)
+
     shape = sh.shapes()
     print len(sh.fields), " fields: ", sh.fields
     print "Loading records..."
