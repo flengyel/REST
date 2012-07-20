73079e5678bed9c5d964257d2d53e64f02fbed70
Florian Lengyel <florian.lengyel@gmail.com>
2012-07-20 05:05:40 -0400

    Additional URIs and functions defined for Discharge and Runoff

--- rgis.py
+++ rgis.py
@@ -8,11 +8,30 @@ import json
 from random import randrange,shuffle
 from preprocess  import loadmaps
 from bfs import traverse, traverseStrahler
-from loaddata import loadIDdictionary
+#from loaddata import loadIDdictionary
+from idmap import IDmap
 
-#maps = loadmaps("NigerShapefiles/NigerRiverDict")
 ordermaps = loadmaps("NigerShapefiles/NigerRiverDictionary")
-idct = loadIDdictionary('NigerShapefiles/ID2q+q25+q50+pop+crop.txt')
+
+myfields = ['ID', 'q_dist_1m_annual' , 'q_dist25_1m_annual', 'q_dist50_1m_annual',	
+                'CropLandAreaAcc','Pop2000','PopAcc2000','Runoff-01','Runoff-02','Runoff-03',
+                'Runoff-04','Runoff-05','Runoff-06','Runoff-07','Runoff-08','Runoff-09',
+                'Runoff-10','Runoff-11','Runoff-12','Runoff25-01','Runoff25-02','Runoff25-03',
+		'Runoff25-04','Runoff25-05','Runoff25-06','Runoff25-07','Runoff25-08',	
+                'Runoff25-09','Runoff25-10','Runoff25-11','Runoff25-12','Runoff50-01',
+        	'Runoff50-02','Runoff50-03','Runoff50-04','Runoff50-05','Runoff50-06',
+                'Runoff50-07','Runoff50-08','Runoff50-09','Runoff50-10','Runoff50-11',
+                'Runoff50-12','Discharge-01','Discharge-02','Discharge-03','Discharge-04',
+           	'Discharge-05','Discharge-06','Discharge-07','Discharge-08','Discharge-09',
+		'Discharge-10','Discharge-11','Discharge-12','Discharge25-01','Discharge25-02',
+		'Discharge25-03','Discharge25-04','Discharge25-05','Discharge25-06',
+		'Discharge25-07','Discharge25-08','Discharge25-09','Discharge25-10',
+		'Discharge25-11','Discharge25-12','Discharge50-01','Discharge50-02',
+		'Discharge50-03','Discharge50-04','Discharge50-05','Discharge50-06',	
+		'Discharge50-07','Discharge50-08','Discharge50-09','Discharge50-10',	
+		'Discharge50-11','Discharge50-12']
+
+idmap = IDmap('NigerShapefiles/NigerRiverActive1m.txt',myfields)
 
 app = Flask(__name__)
 app.wsgi_app = ReverseProxied(app.wsgi_app)
@@ -60,33 +79,73 @@ def AfricaNigerUpstreamOrder(lat,lon,cell,order):
 	return json.dumps({ "Upstream": [] })	
     return json.dumps(traverseStrahler(ordermaps,cellno,ordno))
 
-def cell2json(name,idx,cell):
-    try:
-        lst = idct[cell]
-    except  KeyError:
-        return json.dumps({ name : -9999 })   
-    return json.dumps({ name:lst[idx]})
+def cell2json(cell,name,fld):
+    return json.dumps({ name:idmap.field(cell,fld)})
 
 
-@app.route('/Africa/Niger/Upstream/Discharge/<cell>')
+@app.route('/Africa/Niger/Discharge/Annual/<cell>')
 def AfricaNigerUpstreamQ(cell):
-    return cell2json("Discharge",0,cell)
+    return cell2json(cell,'Discharge','q_dist_1m_annual')
 
-@app.route('/Africa/Niger/Upstream/Discharge25/<cell>')
+@app.route('/Africa/Niger/Discharge25/Annual/<cell>')
 def AfricaNigerUpstreamQ25(cell):
-    return cell2json("Discharge25",1,cell)
+    return cell2json(cell,'Discharge25','q_dist25_1m_annual')
     
-@app.route('/Africa/Niger/Upstream/Discharge50/<cell>')
+@app.route('/Africa/Niger/Discharge50/Annual/<cell>')
 def AfricaNigerUpstreamQ50(cell):
-    return cell2json("Discharge50",2,cell)
-
-@app.route('/Africa/Niger/Upstream/Population/<cell>')
-def AfricaNigerUpstreamPopulation(cell):
-    return cell2json("Population",3,cell)
+    return cell2json(cell,'Discharge50','q_dist50_1m_annual')
 
 @app.route('/Africa/Niger/Upstream/Cropland/<cell>')
 def AfricaNigerUpstreamCropland(cell):
-    return cell2json("Cropland",4,cell)
+    return cell2json(cell,'Cropland','CropLandAreaAcc')
+
+@app.route('/Africa/Niger/Upstream/Population/<cell>')
+def AfricaNigerUpstreamPopulation(cell):
+    return cell2json(cell,"Population", 'PopAcc2000')
+
+@app.route('/Africa/Niger/Population/<cell>')
+def AfricaNigerLocalPopulation(cell):
+    return cell2json(cell,"Population", 'Pop2000')
+
+@app.route('/Africa/Niger/Runoff/Monthly/<cell>/<mm>')
+def AfricaNigerLocalRunoff(cell,mm):
+    fieldname = 'Runoff-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+@app.route('/Africa/Niger/Runoff25/Monthly/<cell>/<mm>')
+def AfricaNigerLocalRunoff25(cell,mm):
+    fieldname = 'Runoff25-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+@app.route('/Africa/Niger/Runoff50/Monthly/<cell>/<mm>')
+def AfricaNigerLocalRunoff50(cell,mm):
+    fieldname = 'Runoff50-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+@app.route('/Africa/Niger/Discharge/Monthly/<cell>/<mm>')
+def AfricaNigerDischargeMonthly(cell,mm):
+    fieldname = 'Discharge-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+@app.route('/Africa/Niger/Discharge25/Monthly/<cell>/<mm>')
+def AfricaNigerDischarge25Monthly(cell,mm):
+    fieldname = 'Discharge25-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+@app.route('/Africa/Niger/Discharge50/Monthly/<cell>/<mm>')
+def AfricaNigerDischarge50Monthly(cell,mm):
+    fieldname = 'Discharge50-'+mm
+    return cell2json(cell, fieldname, fieldname)
+
+
+#@app.route('/Africa/Niger/Upstream/PopByCrop/<cell>')
+#def AfricaNigerUpstreamPopByCrop(cell):
+#    try:
+#        lst = idct[cell]
+#    except  KeyError:
+#        return json.dumps({ name : -9999 })   
+#    return json.dumps({ "PopByCrop":lst[3]/lst[4]})
+    
 
     
 if __name__ == '__main__':
